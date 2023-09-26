7a759c9e25120da353cb7d5deb76b6c91fa62b8b
Tristan <heytristaann@gmail.com>
2023-09-26 17:09:20 -0400

    now prints test completion/fail speed

--- src/testing.c
+++ src/testing.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "types/string.h"
 #include "decimal_to_base.h"
@@ -14,18 +15,25 @@
  * @param count The number of tests
  * @param condition The condition to assert
  */
-void assert(int *count, int condition)
+void assert(int *count, clock_t *start, int condition)
 {
   (*count)++;
 
+  // Get the speed
+  clock_t end = clock();
+  double time_spent = (double)(end - *start) / CLOCKS_PER_SEC;
+
   if (!condition)
   {
-    printf("\033[0;31m[%d/%d] Test failed.\n", *count, TOTAL_TESTS);
+    printf("\033[0;31m[%d/%d] Test failed in %lfs\n", *count, TOTAL_TESTS, time_spent);
   }
   else
   {
-    printf("\033[0;32m[%d/%d] Test passed.\n", *count, TOTAL_TESTS);
+    printf("\033[0;32m[%d/%d] Test passed in %lfs\n", *count, TOTAL_TESTS, time_spent);
   }
+
+  // Reset the start time
+  *start = clock();
 }
 
 /**
@@ -37,15 +45,18 @@ int main(void)
   // Keep track of the number of tests
   int count = 0;
 
+  // Keep track of the time
+  clock_t start = clock();
+
   // decimal_to_base from decimal_to_base.c
-  assert(&count, streq(decimal_to_base(1000, 16), "3E8"));
-  assert(&count, streq(decimal_to_base(1000, 2), "1111101000"));
-  assert(&count, streq(decimal_to_base(1000, 8), "1750"));
-  assert(&count, streq(decimal_to_base(1000, 10), "1000"));
+  assert(&count, &start, streq(decimal_to_base(1000, 16), "3E8"));
+  assert(&count, &start, streq(decimal_to_base(1000, 2), "1111101000"));
+  assert(&count, &start, streq(decimal_to_base(1000, 8), "1750"));
+  assert(&count, &start, streq(decimal_to_base(1000, 10), "1000"));
 
   // base_to_decimal from base_to_decimal.c
-  assert(&count, base_to_decimal("3E8", 16) == 1000);
-  assert(&count, base_to_decimal("1111101000", 2) == 1000);
-  assert(&count, base_to_decimal("1750", 8) == 1000);
-  assert(&count, base_to_decimal("1000", 10) == 1000);
+  assert(&count, &start, base_to_decimal("3E8", 16) == 1000);
+  assert(&count, &start, base_to_decimal("1111101000", 2) == 1000);
+  assert(&count, &start, base_to_decimal("1750", 8) == 1000);
+  assert(&count, &start, base_to_decimal("1000", 10) == 1000);
 }
